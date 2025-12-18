8ec7add4a1722b6a8a55caaf71a55849b19d3658
gmeilkom2-spec <gmeilkom2@gmail.com>
2025-12-18 23:23:53 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 console.log('Wizz Air Roblox site loaded');
+
+
+const form = document.getElementById('bookingForm');
+const result = document.getElementById('bookingResult');
+
+
+if (form) {
+form.addEventListener('submit', (e) => {
+e.preventDefault();
+result.textContent = '✅ Рейс WZR9876 успешно забронирован!';
+result.style.color = '#e6007e';
+form.reset();
+});
+}
